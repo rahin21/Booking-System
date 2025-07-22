5eded5df2a96319d973b96d5685a5ffa41077e8e
Rahin Zaman <mdrahinzaman21@gmail.com>
2025-07-22 08:55:25 +0600

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <Button className="bg-purple">Click me</Button>
+    </div>
+  );
+}
